272692e2556bad674b4c580f2e8c2a811825d1b1
jcerb <brt.jst@gmail.com>
2021-04-08 10:14:30 -0700

    functioning socket.io broadcast

--- server.js
+++ server.js
@@ -11,10 +11,17 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('a user connected');
 });
+
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
       console.log('user disconnected');
+    });
+  });
+  
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
     });
   });
 
