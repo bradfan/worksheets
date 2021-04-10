49b82ac8ca5018b6e9e26896160b7fb8c24ca68f
jcerb <brt.jst@gmail.com>
2021-04-10 12:28:02 -0700

    working sat 12.30

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+// Objects are Passed by Reference
+// In JavaScript, object references are values.
+
+// Because of this, objects will behave like they are passed by reference:
+
+// If a function changes an object property, it changes the original value.
+
+// Changes to object properties are visible (reflected) outside the function.
--- schema.sql
+++
@@ -1,4 +0,0 @@
-CREATE test_db
-
-USE test_db
-
